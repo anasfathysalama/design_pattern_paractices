65f15e1f445203a4f9a3d095480278509a1e7b85
anasfathysalama <salamaanas681@gmail.com>
2026-03-22 22:47:47 +0200

    feat: add pattern defination

--- Structural/index.php
+++ Structural/index.php
@@ -7,6 +7,16 @@ use Structural\Adapter\Adapters\StripeAdapter;
 use Structural\Adapter\PaypalService;
 use Structural\Adapter\StripeService;
 
+
+/**
+ * 
+ * Adapter Pattern 
+ * 
+ * The Adapter Pattern is a structural design pattern that allows objects with incompatible interfaces to work together. It acts as a bridge between two incompatible interfaces,
+ *  enabling them to communicate and collaborate effectively. The adapter pattern is particularly useful when integrating third-party libraries or legacy code into a new system without modifying the existing codebase.
+ * 
+ * 
+ */
 $stripeService = new StripeService();
 $adapter = new StripeAdapter($stripeService);
 echo $adapter->pay(100.0);
